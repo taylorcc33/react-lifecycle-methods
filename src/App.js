26b8372ea2ae2c2fc4006b137c869bf25eb24d34
Taylor Collins <taylorcc33@gmail.com>
2020-12-09 15:59:12 -0700

    added nav bar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Container, Header } from "semantic-ui-react";
 import Counter from "./Counter";
+import Nav from "./Nav";
 
 function App() {
   return (
     <Container>
       <Header as="h1">React Lifecycle Examples</Header>
-      <Counter />
+      <Nav />
     </Container>
   );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,25 @@
 import { useState } from "react";
+import { Menu } from "semantic-ui-react";
+import Counter from "./Counter";
 
 export default (props) => {
   const [showCounter, setShowCounter] = useState(false);
+  const [activeItem, setActiveItem] = useState("");
+
+  return (
+    <>
+      <div>
+        <Menu>
+          <Menu.Item
+            name="showCounter"
+            active={setActiveItem === "showCounter"}
+            onClick={() => setShowCounter(!showCounter)}
+          >
+            {showCounter ? "Hide Counters" : "Show Counters"}
+          </Menu.Item>
+        </Menu>
+      </div>
+      <div>{showCounter && <Counter />}</div>
+    </>
+  );
 };
